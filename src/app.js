e28fbb9eed61a5b840a7f638991d571e03cfc577
RudzeniaP <pavel.rudenya1@gmail.com>
2019-10-06 22:04:33 +0300

    standalone application refactoring

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = require('./router');
 const app = express();
+const bodyParser = require('body-parser');
 const port = process.argv[2] || '3000';
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+    console.log(`A ${req.method} request to ${req.url}`);
+
+    next();
+})
 
 app.use('/', router);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`The app is listening on port ${port}!`));
--- src/router.js
+++ src/router.js
@@ -1,22 +1,119 @@
 const { Router } = require('express');
 const router = Router();
+const crypto = require("crypto");
 
+// NOTE: utils
+const newId = () => {
+  return crypto.randomBytes(16).toString("hex");;
+}
+
+const bookExists = (bookId) => {
+  if (!bookId) {
+    return;
+  }
+
+  return books.find(book => book.id === bookId);
+}
+
+// NOTE: Domain and business logic
+const books = [
+  {
+    id: newId(),
+    title: 'NodeJS in Action',
+    price: 100,
+  },
+  {
+    id: newId(),
+    title: 'You don\'t know JS',
+    price: 110,
+  },
+]
+
+const orders = [
+  {
+    id: newId(),
+    book_id: books[0].id,
+    count: 2,
+  },
+]
+
+const getOrders = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve(orders));
+  })
+}
+
+const createOrder = (order) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (order && bookExists(order.bookId)) {
+        orders.push({
+          id: newId(),
+          bookId: order.bookId,
+          count: order.count,
+        });
+
+        resolve();
+      } else {
+        reject();
+      }
+    });
+  })
+}
+
+const getBooks = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve(books));
+  })
+}
+
+const createBook = (book) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (book) {
+        books.push({
+          id: newId(),
+          title: book.title,
+          price: book.price,
+        });
+
+        resolve();
+      } else {
+        reject();
+      }
+    });
+  })
+}
+
+// NOTE: REST API
 router
   .route('/books')
   .get((req, res) => {
-    res.sendStatus(200);
+    getBooks()
+      .then(books => res.status(200).send(books))
+      .catch(() => res.sendStatus(500));
   })
   .post((req, res) => {
-    res.sendStatus(200);
+    const book = req.body;
+
+    createBook(book)
+      .then(() => res.sendStatus(200))
+      .catch(() => res.sendStatus(400));
   })
 
 router
   .route('/orders')
   .get((req, res) => {
-    res.sendStatus(200);
+    getOrders()
+      .then(orders => res.status(200).send(orders))
+      .catch(() => res.sendStatus(500));
   })
   .post((req, res) => {
-    res.sendStatus(200);
+    const order = req.body;
+
+    createOrder(order)
+      .then(() => res.sendStatus(200))
+      .catch(() => res.sendStatus(400));
   })
 
 module.exports = router;
